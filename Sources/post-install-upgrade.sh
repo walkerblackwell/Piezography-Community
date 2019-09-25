3ac26c394cf5b52eb2a8f09ca6f8e0038b5c6a50
Walker Blackwell <wblackwell@gmail.com>
2019-09-25 08:22:51 -0400

    update

--- Sources/post-install-upgrade.sh
+++ Sources/post-install-upgrade.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 rm /Library/ColorSync/Profiles/SoftProofing
-ditto /Applications/Piezography/ICCs/SOFT/ /Library/ColorSync/Profiles/SoftProofing/
+ditto /Applications/Piezography/ICCs/Soft/ /Library/ColorSync/Profiles/SoftProofing/
 cp /Applications/Piezography/ICCs/SoftProofing/* /Library/ColorSync/Profiles/SoftProofing/
 rm -R /Applications/Piezography/ICCs/SoftProofing
 ln -s /Library/ColorSync/Profiles/SoftProofing /Applications/Piezography/ICCs/SoftProofing
-rm -R /Applications/Piezography/ICCs/SOFT
+rm -R /Applications/Piezography/ICCs/Soft
+
+rm /Library/ColorSync/Profiles/Printing
+ditto /Applications/Piezography/ICCs/Print/ /Library/ColorSync/Profiles/Printing/
+cp /Applications/Piezography/ICCs/Printing/* /Library/ColorSync/Profiles/Printing/
+rm -R /Applications/Piezography/ICCs/Printing
+ln -s /Library/ColorSync/Profiles/Printing /Applications/Piezography/ICCs/Printing
+rm -R /Applications/Piezography/ICCs/Print
+
 open /Applications/Piezography
-open /Applications/Piezography/Welcome.pdf
-open /Applications/Piezography/ReadMe.pdf
